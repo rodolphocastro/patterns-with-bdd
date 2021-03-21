51f7ef31bd2a4176dffffc43c1479297c864b550
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-03-20 21:50:15 -0300

    chore: Init Books.Lib

---
+++ src/Books.Lib/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Books.Lib.Entities
+{
+    /// <summary>
+    /// A book written by an Author.
+    /// </summary>
+    public class Book
+    {
+        // TODO: Scaffold with Stubs
+    }
+}
